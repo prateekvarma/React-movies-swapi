4e39664230a60e71b61d6ee461943f35f7a64a61
Prateek Varma <prtk.varma@gmail.com>
2022-09-11 17:49:53 -0600

    Manual error handling for API's error response

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json(); //converts json data to json object
+    setError(null); //just to reset any previous errors
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
 
-    const transformedData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedData);
-    setIsLoading(false);
+      if (!response.ok) {
+        throw new Error("Something went wrong with the API!");
+      }
+
+      const data = await response.json(); //converts json data to json object
+
+      const transformedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message); //this error message will be the same as we manually wrote above
+    }
+    setIsLoading(false); // Loading is diabled in the end, no matter success or error
   }
 
   return (
@@ -30,9 +41,12 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />} {/*if It's NOT loading & there are some movies, then show the paragraph*/}
-        {!isLoading && movies.length === 0 && <p>No movies found.</p> } {/*if It's NOT loading & movies are 0, then show the paragraph*/}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}{" "}
+        {/*if It's NOT loading & there are some movies, then show the paragraph*/}
+        {!isLoading && movies.length === 0 && <p>No movies found.</p>}{" "}
+        {/*if It's NOT loading & movies are 0, then show the paragraph*/}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
